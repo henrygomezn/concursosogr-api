1df837903add0ff1b808dd34b76dc72ce577b42b
henrygomezn <h.gomez01@ufromail.cl>
2021-08-12 17:50:17 -0400

    cors solucionado - Agregar concurso seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@ cliente = Rol.create(nombre: "cliente")
 visitante = Rol.create(nombre: "visitante")
 
 Usuario.create(correo: "henry.gomez@ogr.cl", password: "123456", rol: administrador, rut: "18.527.306-7")
+Concurso.create(nombre: "Escritores Jovenes", descripcion: "Un concurso de OGR", bases: "A llenar")
 
 
