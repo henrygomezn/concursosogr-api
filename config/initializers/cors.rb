341a0ed05781323a6cd436f54ca1001fcdd358ba
henrygomezn <h.gomez01@ufromail.cl>
2021-08-12 21:18:37 -0400

    https sin /

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.escritoresjovenes.com/'
+    origins 'https://www.escritoresjovenes.com'
 
     resource '*',
       headers: :any,
